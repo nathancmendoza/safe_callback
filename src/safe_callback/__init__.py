9ba5aaeda0f4fd115193fc438d8fef0ce0d252e8
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-02-20 11:46:05 -0800

    Add cookiecutter files

---
+++ src/safe_callback/__init__.py
@@ -0,0 +1,5 @@
+"""
+    :module_name: safe_callback
+    :module_summary: A declarative approach to exception handling in python
+    :module_author: Nathan Mendoza
+"""
---
+++ test/unit/test_safe_callback.py
@@ -0,0 +1,19 @@
+"""Tests for safe_callback"""
+
+import pytest
+
+import safe_callback
+
+def test_vacuous():
+    """A test that passes vacuously"""
+    pass
+
+def test_exception():
+    """A test that checks if an exception was raised"""
+    with pytest.raises(ValueError):
+        raise ValueError()
+
+@pytest.mark.parametrize("x, y", [(a, b) for a in range(10) for b in range(10)])
+def test_parametrize(x, y):
+    """Test cases can be parametrized to test multiple inputs at once"""
+    assert x + y == y + x
