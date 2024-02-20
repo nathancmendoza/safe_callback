9992a4ea0cb0148afa07581ede481e0654fdee99
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-02-20 13:21:10 -0800

    Initial test suite to show proof on concept on methods and functions

--- test/unit/test_safe_callback.py
+++ test/unit/test_safe_callback.py
@@ -1,19 +1,60 @@
 """Tests for safe_callback"""
+from __future__ import annotations
 
 import pytest
 
 import safe_callback
 
-def test_vacuous():
-    """A test that passes vacuously"""
-    pass
 
-def test_exception():
-    """A test that checks if an exception was raised"""
-    with pytest.raises(ValueError):
-        raise ValueError()
+@pytest.fixture
+def decorated_function():
 
-@pytest.mark.parametrize("x, y", [(a, b) for a in range(10) for b in range(10)])
-def test_parametrize(x, y):
-    """Test cases can be parametrized to test multiple inputs at once"""
-    assert x + y == y + x
+    def handle_function_error(e, x, y):
+        return f"Cannot divide {x} and {y} because denominator is 0!"
+
+    @safe_callback.safecallback({
+        ZeroDivisionError: handle_function_error
+    })
+    def divide(x, y):
+        return x / y
+
+    return divide
+
+
+@pytest.fixture
+def decorated_method():
+
+    def handle_method_error(self, e, x, y):
+        return f"Cannot divide {x} and {y} because denominator is 0!"
+
+    class Calc:
+
+        @safe_callback.safecallback({
+            ZeroDivisionError: handle_method_error
+        })
+        def divide(self, x, y):
+            return x / y
+
+    return Calc()
+
+
+def test_decorated_function_returns_expected_result_on_ok(decorated_function):
+    assert decorated_function(4, 2) == 2.0
+
+
+def test_decorated_function_calls_error_function_on_exception(
+    decorated_function
+):
+    assert decorated_function(
+        4, 0) == "Cannot divide 4 and 0 because denominator is 0!"
+
+
+def test_decorated_method_returns_expected_result_on_ok(decorated_method):
+    assert decorated_method.divide(4, 2) == 2.0
+
+
+def test_decorated_method_returns_calls_error_function_on_exception(
+    decorated_method
+):
+    assert decorated_method.divide(
+        4, 0) == "Cannot divide 4 and 0 because denominator is 0!"
