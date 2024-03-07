96638186720c559a34e26ba63e5a7b4256144180
Nathan Mendoza <74679703+nathancmendoza@users.noreply.github.com>
2024-03-07 14:41:50 -0800

    Update errors test case to align with new default behavior

--- test/unit/test_safe_callback.py
+++ test/unit/test_safe_callback.py
@@ -1,7 +1,9 @@
 """Tests for safe_callback"""
 from __future__ import annotations
 
+from unittest.mock import patch
 import pytest
+import io
 
 import safe_callback
 
@@ -10,7 +12,7 @@ import safe_callback
 def decorated_function():
 
     def handle_function_error(e):
-        return "Cannot divide because denominator is 0!"
+        print("Cannot divide because denominator is 0!")
 
     @safe_callback.safecallback({
         ZeroDivisionError: handle_function_error
@@ -25,7 +27,7 @@ def decorated_function():
 def decorated_method():
 
     def handle_method_error(e):
-        return "Cannot divide because denominator is 0!"
+        print("Cannot divide because denominator is 0!")
 
     class Calc:
 
@@ -42,19 +44,25 @@ def test_decorated_function_returns_expected_result_on_ok(decorated_function):
     assert decorated_function(4, 2) == 2.0
 
 
+@patch('sys.stdout', new_callable=io.StringIO)
 def test_decorated_function_calls_error_function_on_exception(
+    mock_stdout,
     decorated_function
 ):
-    assert decorated_function(
-        4, 0) == "Cannot divide because denominator is 0!"
+    assert decorated_function(4, 0) is None
+    expected_output = "Cannot divide because denominator is 0!\n"
+    assert mock_stdout.getvalue() == expected_output
 
 
 def test_decorated_method_returns_expected_result_on_ok(decorated_method):
     assert decorated_method.divide(4, 2) == 2.0
 
 
+@patch('sys.stdout', new_callable=io.StringIO)
 def test_decorated_method_returns_calls_error_function_on_exception(
+    mock_stdout,
     decorated_method
 ):
-    assert decorated_method.divide(
-        4, 0) == "Cannot divide because denominator is 0!"
+    assert decorated_method.divide(4, 0) is None
+    expected_output = "Cannot divide because denominator is 0!\n"
+    assert mock_stdout.getvalue() == expected_output
